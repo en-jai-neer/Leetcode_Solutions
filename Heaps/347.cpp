010c3ddec17279cb4fb8e9993dc1f2d15213a77d
Jai Jain <40489318+en-jai-neer@users.noreply.github.com>
2020-05-23 01:36:02 +0530

    Create 347.cpp

---
+++ Heaps/347.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<int> ans;
+        unordered_map<int, int> mp;
+        for(int i: nums)
+            mp[i]++;
+        priority_queue<pair<int, int>> pq;
+        for(auto i: mp)
+            pq.push({i.second, i.first});
+        for(int i = 0; i < k; i++)
+        {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
+};
