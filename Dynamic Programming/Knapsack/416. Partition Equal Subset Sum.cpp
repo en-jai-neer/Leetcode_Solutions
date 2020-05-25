659eddc641c1949a2d6fecac8aaf9b4bc87d562d
Jai Jain <40489318+en-jai-neer@users.noreply.github.com>
2020-05-26 01:10:55 +0530

    Create 416. Partition Equal Subset Sum.cpp

---
+++ Dynamic Programming/Knapsack/416. Partition Equal Subset Sum.cpp
@@ -0,0 +1,99 @@
+// 2D Top down (Memoization) - [Although the Time Complexity of all the solutions is same but top down is faster because it does not 
+// calculate all n*sum states. It only calculates the states that are necessary.
+class Solution {
+private:
+    int topDown(vector<int>& nums, int n, int sum)
+    {
+        if(sum == 0)
+            return true;
+        if(n == 0)
+            return false;
+        if(dp[n][sum] != -1)
+            return dp[n][sum];
+        if(sum - nums[n-1] >= 0)
+            dp[n][sum] = topDown(nums, n-1, sum - nums[n-1]) || topDown(nums, n-1, sum);
+        else
+            dp[n][sum] = topDown(nums, n-1, sum);
+        return dp[n][sum];
+    }
+public:
+    vector<vector<int>> dp;
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        for(int i: nums)
+            sum += i;
+        if(sum % 2 == 1)
+            return false;
+        sum /= 2;
+        dp.clear();
+        dp.resize(n+1, vector<int> (sum+1, -1));
+        return topDown(nums, n, sum);
+    }
+};
+
+//*********************************************************************************************************************************//
+//2D Bottom Up
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        for(int i: nums)
+            sum += i;
+        if(sum % 2 == 1)
+            return false;
+        sum /= 2;
+        vector<vector<bool>> dp(n+1, vector<bool> (sum+1, false));
+        for(int i = 0; i <= n; i++)
+        {
+            dp[i][0] = true;
+        }
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= sum; j++)
+            {
+                if(j - nums[i-1] >= 0)
+                {
+                    dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                }
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[n][sum];
+    }
+};
+
+//*********************************************************************************************************************************//
+//1D Bottom Up
+// Every loop of nums refreshes dp array. We might get dp[i] from dp[i-num] whose index is smaller than i. If we increase the index of // sum from 0 to sum, we will get dp[i] from dp[i-num] , while dp[i-num] has been updated in this loop. This dp[i-num] is not the number // we got from the previous loop.
+
+// So why would we do this? This is because the numbers in nums can only be used once. If we can choose each number several times, we 
+// have to increase i from 0 to sum (Unbounded Knapsack - Coin Change problems)
+
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        for(int i: nums)
+            sum += i;
+        if(sum % 2 == 1)
+            return false;
+        sum /= 2;
+        vector<bool> dp(sum+1);
+        dp[0] = true;
+        for(int num: nums)
+        {
+            for(int i = sum; i >= 0; i--)
+            {
+                if(i - num>= 0)
+                {
+                    dp[i] = dp[i] || dp[i-num];
+                }
+            }
+        }
+        return dp[sum];
+    }
+};
