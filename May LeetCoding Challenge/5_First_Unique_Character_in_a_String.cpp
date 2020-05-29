cae55571eb6791d57f0d5680fafa1d5d6b2fa610
Jai Jain <40489318+en-jai-neer@users.noreply.github.com>
2020-05-29 09:38:13 +0530

    Create 5_First_Unique_Character_in_a_String.cpp

---
+++ May LeetCoding Challenge/5_First_Unique_Character_in_a_String.cpp
@@ -0,0 +1,29 @@
+Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+Examples:
+
+s = "leetcode"
+return 0.
+
+s = "loveleetcode",
+return 2.
+Note: You may assume the string contain only lowercase letters.
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char, int> mp;
+        if(s.size() == 0)
+            return -1;
+        for(char c: s)
+        {
+            mp[c]++;
+        }
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(mp[s[i]] == 1)
+                return i;
+        }
+        return -1;
+    }
+};
