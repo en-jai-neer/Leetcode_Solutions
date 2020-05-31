cca482fbf08949b0f42e31c00a6cd2e512e9061c
Jai Jain <40489318+en-jai-neer@users.noreply.github.com>
2020-06-01 00:47:43 +0530

    Create 29_Course Schedule.cpp

---
+++ May LeetCoding Challenge/29_Course Schedule.cpp
@@ -0,0 +1,60 @@
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses-1.
+
+Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+
+Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+
+ 
+
+Example 1:
+
+Input: numCourses = 2, prerequisites = [[1,0]]
+Output: true
+Explanation: There are a total of 2 courses to take. 
+             To take course 1 you should have finished course 0. So it is possible.
+Example 2:
+
+Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+Output: false
+Explanation: There are a total of 2 courses to take. 
+             To take course 1 you should have finished course 0, and to take course 0 you should
+             also have finished course 1. So it is impossible.
+ 
+
+Constraints:
+
+The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
+You may assume that there are no duplicate edges in the input prerequisites.
+1 <= numCourses <= 10^5
+
+class Solution {
+private:
+    void dfs(vector<int> adj[], int s, vector<int>& color)
+    {
+        color[s] = 1;
+        for(int i = 0; i < adj[s].size(); i++)
+        {
+            int v = adj[s][i];
+            if(color[v] == 1)
+                ans = false;
+            else if(color[v] == 0)
+                dfs(adj, v, color);
+        }
+        color[s] = 2;
+    }
+    
+public:
+    int ans = true;
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> adj[numCourses];
+        vector<int> color(numCourses);
+        for(auto edge: prerequisites)
+        {
+            adj[edge[0]].push_back(edge[1]);
+        }
+        for(int i = 0; i < numCourses; i++)
+            if(color[i] == 0)
+                dfs(adj, i, color);
+        return ans;
+    }
+};
