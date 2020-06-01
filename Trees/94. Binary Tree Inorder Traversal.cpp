f7b02370f02955ac8da9668c5032d9c947bda722
Jai Jain <40489318+en-jai-neer@users.noreply.github.com>
2020-06-01 16:37:52 +0530

    Update 94. Binary Tree Inorder Traversal.cpp

--- Trees/94. Binary Tree Inorder Traversal.cpp
+++ Trees/94. Binary Tree Inorder Traversal.cpp
@@ -16,3 +16,25 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        stack<TreeNode* > st;
+        TreeNode* cur = root;
+        while(!st.empty() || cur != NULL)
+        {
+            while(cur != NULL)
+            {
+                st.push(cur);
+                cur = cur->left;
+            }
+            cur = st.top();
+            ans.push_back(cur->val);
+            st.pop();
+            cur = cur->right;
+        }
+        return ans;
+    }
+};
