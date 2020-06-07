ab7fe8a1a98840515b38c2795b5b1d6f273e2d3b
Jai Jain <40489318+en-jai-neer@users.noreply.github.com>
2020-06-07 08:15:13 +0530

    Update 112. Path Sum.cpp

--- Trees/Path Sum/112. Path Sum.cpp
+++ Trees/Path Sum/112. Path Sum.cpp
@@ -16,4 +16,50 @@ public:
     }
 };
 
-//Iterative
+//Iterative DFS (preorder)
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if(root == NULL)
+            return false;
+        stack<pair<TreeNode *, int>> st;
+        st.push({root, root->val});
+        while(!st.empty())
+        {
+            int k = st.top().second;
+            TreeNode* cur = st.top().first;
+            st.pop();
+            if(cur->left == NULL && cur->right == NULL && sum == k)
+                return true;
+            if(cur->right)
+                st.push({cur->right, k + cur->right->val});
+            if(cur->left)
+                st.push({cur->left, k + cur->left->val});
+        }
+        return false;
+    }
+};
+
+//Iterative BFS
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if(root == NULL)
+            return false;
+        queue<pair<TreeNode *, int>> q;
+        q.push({root, root->val});
+        while(!q.empty())
+        {
+            int k = q.front().second;
+            TreeNode* cur = q.front().first;
+            q.pop();
+            if(cur->left == NULL && cur->right == NULL && sum == k)
+                return true;
+            if(cur->left)
+                q.push({cur->left, k + cur->left->val});
+            if(cur->right)
+                q.push({cur->right, k + cur->right->val});
+        }
+        return false;
+    }
+};
